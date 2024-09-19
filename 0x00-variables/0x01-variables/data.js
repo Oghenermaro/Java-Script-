749bd3d15fe6f965d88de4683716ddcc96f58633
Oghenemaro <obogasmaro@gmail.com>
2024-09-19 12:29:27 +0100

    add

---
+++ 0x00-variables/0x01-variables/data.js
@@ -0,0 +1,19 @@
+//Boolean
+let is_Admin = true;
+let is_user = false;
+
+console.log(is_Admin);
+console.log(is_user);
+
+//typesof function
+console.log(typeof(is_Admin));
+result = typeof(is_Admin);
+console.log(result);
+
+//undefine
+let name;
+console.log(name)
+
+//null
+let name2 = null;
+console.log(name2)
---
+++ 0x00-variables/index.js
@@ -0,0 +1,9 @@
+//case sensitive
+
+const userName = "favour";
+const UserName = "kome";
+const username = "maro";
+
+console.log(userName);
+console.log(UserName);
+console.log(username);
