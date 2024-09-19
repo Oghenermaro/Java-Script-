e6aeb5e048fb02718bc8cf0b7bddab7fd34bb152
Oghenemaro <obogasmaro@gmail.com>
2024-09-19 13:58:03 +0100

    operator

---
+++ 0x01-operator/index.js
@@ -0,0 +1,22 @@
+//arithmetic operator
+let a = 4;
+let b = 2;
+
+let addition = a + b;
+let sub = a - b;
+console.log("addition of " + a + " and " + b + " is: " + addition)
+
+let c = 8;
+let d = 4;
+
+let mutiplication = c * d;
+let division = c / d;
+console.log("mutiplication of " + c + " and" + d + " is: " + mutiplication)
+
+//caulation
+let days = 800;
+let numberOfHours  = days * 24;
+let numberOfMinutes = numberOfHours * 60;
+let numberOfSecons = numberOfMinutes * 60;
+
+console.log("there are " + numberOfHours + " hours in " +  numberOfMinutes + " minutes in " + " and " + numberOfSecons + " in " + days + " days ")
